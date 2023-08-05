b906624c9d754ad4794fedcd181c0ebec68d5da6
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-08-05 11:27:10 +0530

    added a html file for testing

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ const people = [
 
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
+const output = inventors.filter();
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors first and last names
