65260415d74bc3d867dbcfb002dc8636a0eb00fb
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-08-05 13:51:54 +0530

    day 7 solutions done!

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const output = inventors.filter((x) => x.year >= 1500 && x.year < 1600);
 console.log(output);
 
 // Array.prototype.map()
-// gives the same length 
+// gives the same length
 // 2. Give us an array of the inventors first and last names
 const output1 = inventors.map((x) => x.first + " " + x.last);
 console.log(output1);
@@ -76,25 +76,25 @@ console.log(output1);
 //     return -1;
 //   }
 // })
-const output2=inventors.sort((a,b)=>a.year>b.year? 1:-1);
+const output2 = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
 console.log(output2);
 // 4. How many years did all the inventors live all together?
 const output3 = inventors.reduce(function (acc, curr) {
   return acc + (curr.passed - curr.year);
-
 }, 0);
 console.log(output3);
 // 5. Sort the inventors by years lived
-const output4=inventors.sort((a,b)=>(a.passed-a.year)>(b.passed-b.year)? -1:1);
+const output4 = inventors.sort((a, b) =>
+  a.passed - a.year > b.passed - b.year ? -1 : 1
+);
 console.log(output4);
-
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
-const output5=people.sort(function(a,b){
-  const[last,first] =a.split(", ");
-  const[last1,first1]=b.split(", ");
-  return a.last>b.last1? -1:1;
+const output5 = people.sort(function (a, b) {
+  const [last, first] = a.split(", ");
+  const [last1, first1] = b.split(", ");
+  return a.last > b.last1 ? -1 : 1;
 });
 console.log(output5);
 
@@ -125,3 +125,40 @@ const output7 = data.reduce(function (acc, cur) {
   return acc;
 }, {});
 console.log(output7);
+
+// DAY 7
+const people1 = [
+  { name: "Wes", year: 1988 },
+  { name: "Kait", year: 1986 },
+  { name: "Irv", year: 1970 },
+  { name: "Lux", year: 2015 },
+];
+
+const comments = [
+  { text: "Love this!", id: 523423 },
+  { text: "Super good", id: 823423 },
+  { text: "You are the best", id: 2039842 },
+  { text: "Ramen is my fav food ever", id: 123523 },
+  { text: "Nice Nice Nice!", id: 542328 },
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const out=people1.some(x=>(2023-x.year)>=19);
+console.log(out);
+// Array.prototype.every() // is everyone 19 or older?
+const out1=people1.every(x=>(2023-x.year)>=19);
+console.log(out1);
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+const out2=comments.find(x=>x.id==823423);
+console.log(out2);
+const index = comments.findIndex(comment => comment.id == 823423);
+console.log(index);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+const out3=comments.filter(x=>x.id!=823423);
+console.log(out3);
